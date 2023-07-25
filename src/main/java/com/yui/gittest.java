98b4236ea8fb24afdf1659b86e5cb9d3b1e70824
yuwzhao <yuwzhao16@163.com>
2023-07-25 16:15:29 +0800

    hot-fix commit2

--- src/main/java/com/yui/gittest.java
+++ src/main/java/com/yui/gittest.java
@@ -4,5 +4,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hello, git2");
         System.out.println("hello, git3");
+        System.out.println("hello, git4");
     }
 }
