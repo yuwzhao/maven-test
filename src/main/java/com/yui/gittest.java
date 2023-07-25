1c09eff0b2341fa0d82c43d770c95ba955adaec2
yuwzhao <yuwzhao16@163.com>
2023-07-25 16:06:52 +0800

    second commit

--- src/main/java/com/yui/gittest.java
+++ src/main/java/com/yui/gittest.java
@@ -2,6 +2,6 @@ package com.yui;
 
 public class gittest {
     public static void main(String[] args) {
-        System.out.println("hello, git");
+        System.out.println("hello, git2");
     }
 }
